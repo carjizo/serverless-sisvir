94036b54ffe46083fc0ff1dbcd90ee43c5b23b6a
carjiso <cjimenez@indigitalsolutions.com>
2025-03-09 10:27:45 -0500

    init project

---
+++ sam/functions/core/login/app.py
@@ -0,0 +1,13 @@
+import logging
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+environment = os.environ
+
+def lambda_handler(event, _):
+    logger.info(f"event: {event}")
+    logger.info(f"environment: {environment}")
+    print("event", event)
+    method = event['httpMethod']
+    path = event['requestContext']['resourcePath']
